1b5d508b7fc7cb2dd2688d22d09a354c732922f3
Farhan900 <109672555+Farhan900@users.noreply.github.com>
2022-07-20 20:16:45 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,13 +2,13 @@ const qrku = "https://i.ibb.co/MnbqZ0N/qr-Aguz-Familia-07-06-22-1654614928.png"
 
 let handler = async (m, { conn, usedPrefix }) => conn.sendButtonImg(m.chat, qrku, `
 ╭─「 Donasi • Dana 」
-│ • Axis [083813417529]
+│ • Smarfren [+62 882-0064-85504]
 │ • Dana  []
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? 6282146218274
+│ > Ingin donasi? wa.me/62882006485504
 │ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _atau beli *RDP/PANEL* agar bot bisa jalan_
 │ _24jam tanpa kendala_
 ╰────
 `.trim(), wm, '𝐌𝐄𝐍𝐔', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
